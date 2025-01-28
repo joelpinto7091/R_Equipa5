fd7f06a81749a4ae5d29624cc52f9bd95c4e3d76
joelpinto7091 <50111@alunos.upt.pt>
2025-01-28 23:28:44 +0000

    Add files via upload
    classes model

---
+++ src/main/java/com/joel/recursolp/RecursolpApplication.java
@@ -0,0 +1,13 @@
+package com.joel.recursolp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecursolpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecursolpApplication.class, args);
+	}
+
+}
